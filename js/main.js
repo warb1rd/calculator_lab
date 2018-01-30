b5d4d088e46eeff4aa54d3bdaefab8a669514725
Katie Goines <30757403+katiegoines@users.noreply.github.com>
2018-01-30 14:12:27 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Write an evaluate() function (do not use the eval() function)
+
+// Write a function(s) for what should happen when you click on any digit
+
+// Write a function(s) for what should happen when you click on any operator
